b3aaf4602bc058c32021949683de61fd5e6c079c
Cameron Lamb <jafacakes2011@hotmail.co.uk>
2022-03-01 17:17:11 +0000

    Add missing types

--- django_hawk/tests/views.py
+++ django_hawk/tests/views.py
@@ -1,11 +1,11 @@
-from django.http import HttpResponse, JsonResponse
+from django.http import HttpRequest, HttpResponse, JsonResponse
 from django.utils.decorators import decorator_from_middleware
 from django_hawk.middleware import HawkResponseMiddleware
 from django_hawk.utils import DjangoHawkAuthenticationFailed, authenticate_request
 
 
 @decorator_from_middleware(HawkResponseMiddleware)
-def simple_view(request):
+def simple_view(request: HttpRequest) -> HttpResponse:
     try:
         authenticate_request(request=request)
     except DjangoHawkAuthenticationFailed as e:
